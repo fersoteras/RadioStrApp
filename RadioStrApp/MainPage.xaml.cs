f3d0eb5d02a3ab057b8917b26244b9f424e17539
Fernando Soteras <fernando.soteras@coderio.co>
2020-09-07 20:42:18 -0300

    Add project files.

---
+++ RadioStrApp/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RadioStrApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Task.Run(AnimateBackground);
+        }
+
+        private async void AnimateBackground()
+        {
+            Action<double> forward = input => bgGradient.AnchorX = input;
+            Action<double> backward = input => bgGradient.AnchorY = input;
+
+            while (true)
+            {
+                // this two animations will work at the same time they are asyncronous.
+                bgGradient.Animate(name: "forward", callback: forward, start: 0, end: 1, length: 5000, easing: Easing.SinIn);
+                await Task.Delay(5000);
+                bgGradient.Animate(name: "backward", callback: backward, start: 1, end: 0, length: 5000, easing: Easing.SinIn);
+            }
+
+
+        }
+    }
+}
